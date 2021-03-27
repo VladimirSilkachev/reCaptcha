109187d53be57b9013038ca8893997bcdeea0aae
KirillPopov001 <55420132+KirillPopov001@users.noreply.github.com>
2021-03-27 12:55:25 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="myrk.css" rel="stylesheet" type="text/css">
+</head>
+<body background="5a9ee783c3f3a.jpg">
+<form id="head" action="proverka.php" method="post" class="ramka">
+
+    <div >
+        <img src="mail.php">
+    </div>
+ 
+    <div >
+        <label  for="message">Input text</label>
+        <input type="text" name="message">
+    </div>
+
+    <div>
+        <input id="knopka" type="submit" name="send">
+    </div>
+
+</form>
+
+</body>
+</html>
